10025c1718efad59c0d68a2e3a48ed488454e5e2
deanone <athanasios.salamanis@gmail.com>
2024-07-06 17:03:23 +0300

    Minor stylistic changes.

--- src/Util.java
+++ src/Util.java
@@ -5,23 +5,24 @@ import java.util.List;
 import java.util.Random;
 
 public class Util {
-    public static void createFileOfRandomUnsortedIntegersWithDuplicates(int n, int minValue, int maxValue, String filename) {
-        Random rn = new Random();
-        List<Integer> numbersOfToBeSorted= new ArrayList<>();
+    public static void createFileOfRandomUnsortedIntegersWithoutDuplicates(int n, int min, int max, String filename) {
+        Random random = new Random();
+        List<Integer> numbersToBeSorted= new ArrayList<>();
         int count = 0;
         try {
             FileWriter myWriter = new FileWriter(filename);
             while (count < n) {
-                int val = minValue + rn.nextInt(maxValue - minValue + 1);
-                if (numbersOfToBeSorted.contains(val)) {
+                int val = random.nextInt(max - min) + min;
+                if (numbersToBeSorted.contains(val)) {
                     continue;
                 }
                 myWriter.write(String.valueOf(val) + "\n");
+                numbersToBeSorted.add(val);
                 count++;
             }
             myWriter.close();  
-        } catch(IOException ioe) {
-            ioe.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
         }
     }
 }
